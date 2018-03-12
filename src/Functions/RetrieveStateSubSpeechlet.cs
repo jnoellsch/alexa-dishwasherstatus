6478b677144addec1ee5abb3eba57d10bad7ac56
Jim Noellsch <jnoellsch@rightpoint.com>
2018-03-11 22:51:03 -0500

    Adjusted flow/slot logic for status retrieval.

--- src/Functions/RetrieveStateSubSpeechlet.cs
+++ src/Functions/RetrieveStateSubSpeechlet.cs
@@ -4,6 +4,7 @@
     using System.Threading.Tasks;
     using AlexaSkillsKit.Slu;
     using AlexaSkillsKit.Speechlet;
+    using AlexaSkillsKit.UI;
 
     public class RetrieveStateSubSpeechlet : ISubSpeechlet
     {
@@ -14,9 +15,31 @@
 
         public Intent Intent { get; }
 
-        public Task<SpeechletResponse> RespondAsync()
+        public async Task<SpeechletResponse> RespondAsync()
         {
-            throw new NotImplementedException();
+            this.Intent.Slots.TryGetValue("State", out var stateSlot);
+            string text;
+
+            // if slot mentioned, user is trying to be specific in the ask. 
+            // otherwise, it's generic. 
+            if (this.SlotWasSpecified(stateSlot))
+            {
+                string randoYesNo = "yes";
+                text = $"You asked if the dishes are {stateSlot?.Value}. The answer is {randoYesNo}.";
+            }
+            else
+            {
+                string randoStatus = "clean";
+                text = $"You asked about the status of the dishes. The status of your dishwasher is {randoStatus}.";
+            }
+
+            var speech = new PlainTextOutputSpeech() { Text = text };
+            var card = new SimpleCard() { Title = "Dishwasher Status", Content = text };
+            var response = new SpeechletResponse { OutputSpeech = speech, Card = card, ShouldEndSession = false };
+
+            return response;
         }
+
+        private bool SlotWasSpecified(Slot slot) => !string.IsNullOrEmpty(slot?.Value);
     }
 }
