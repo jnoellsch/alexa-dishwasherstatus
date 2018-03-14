bff8f96e6b64a55c290cc60d326cc3deed3f4aa0
Jim Noellsch <jnoellsch@rightpoint.com>
2018-03-13 23:09:19 -0500

    Dirty status = -1, not 1 like clean (copy/past durp).

--- src/Data/Models/DirtyStatus.cs
+++ src/Data/Models/DirtyStatus.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public class DirtyStatus : Status
     {
-        public override int Code => 1;
+        public override int Code => -1;
 
         public override string Text => "dirty";
     }
