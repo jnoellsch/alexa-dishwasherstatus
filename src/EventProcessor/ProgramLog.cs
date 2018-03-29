345e1ee63bec0e2d794d193a7c808a5557aa660c
Jim Noellsch <jnoellsch@rightpoint.com>
2018-03-28 20:54:16 -0500

    Moved log code.

---
+++ src/EventProcessor/ProgramLog.cs
@@ -0,0 +1,34 @@
+﻿namespace Alexa.EventProcessor
+{
+    using System;
+    using Alexa.Data.Repositories;
+    using Microsoft.Azure.ServiceBus;
+
+    public class ProgramLog
+    {
+        public void Start()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Dishwasher status queue watcher started!");
+        }
+
+        public void MessageReceived(WashcycleMessageDto dto)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Message received!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"User: {dto.UserId}");
+            Console.WriteLine($"Completes at: {dto.CycleCompletesAt}");
+            Console.WriteLine($"Phone: {dto.Phone}");
+        }
+
+        public void ExceptionOccured(ExceptionReceivedEventArgs args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Message handler encounted an exception. :'(");
+            Console.WriteLine($"{args.Exception}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
